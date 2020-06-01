c0b45a6727f9f63502a39df6bae67c976599c3f3
Arif <arifkhan65@gmail.com>
2020-06-01 11:45:00 +0200

    2dn attempt

--- number7.py
+++ number7.py
@@ -1,5 +1,4 @@
-line=[]
-for i in range (2000,3201):
-    if (i%7==0) and (i%5!=0):
-        line.append(str(i))
-        print(','.join(line))
+r1=int(input('start range: '))
+r2=int(input('end range: '))
+line=[z for z in range(r1, r2+1) if z % 7 ==0 and z % 5 != 0]
+print(line)
